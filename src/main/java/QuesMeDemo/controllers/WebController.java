f2f384933081f468dafcc7814b7ff0c893629fd9
valer <lera.1706@mail.ru>
2022-04-18 12:42:30 +0400

    html начало1

--- src/main/java/QuesMeDemo/controllers/WebController.java
+++ src/main/java/QuesMeDemo/controllers/WebController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-
+/*
     @GetMapping("/")
     public String test() {
         return "test";
-    }
+    }*/
 }
