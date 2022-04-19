835505a166f2af5c5bddadd5e2690b158525cfee
vallarla <lera.1706@mail.ru>
2022-04-20 00:06:53 +0400

    html гг

--- src/main/java/QuesMeDemo/DemoApplication.java
+++ src/main/java/QuesMeDemo/DemoApplication.java
@@ -1,9 +1,12 @@
 package QuesMeDemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"controllers"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/QuesMeDemo/controllers/WebController.java
+++ src/main/java/QuesMeDemo/controllers/WebController.java
@@ -1,13 +1,36 @@
 package QuesMeDemo.controllers;
 
+import QuesMeDemo.entities.AdminEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebController {
-/*
-    @GetMapping("/")
-    public String test() {
-        return "test";
-    }*/
+    private static List<AdminEntity> persons = new ArrayList<AdminEntity>();
+
+    static {
+        persons.add(new AdminEntity("QuesMe", "Global Admin", "87654321"));
+        persons.add(new AdminEntity("IntelliJ IDEA", "IDE2021", "202132"));
+    }
+
+    // ​​​​​​​
+    // Вводится (inject) из application.properties.
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
 }
